223a19af1bcc295324114c6eaf8552b0f54f2f95
Amrit Kumar Gupta <142594159+amritg2121@users.noreply.github.com>
2025-09-30 15:16:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors');
 const app = express()
 const port = 5000
 const mongoDB = require("./db")
@@ -20,7 +19,8 @@ app.use('/api', require("./Routes/DisplayData"));
 app.use('/api', require("./Routes/OrderData"));
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
